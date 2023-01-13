ea8879dfef1a62a0cbf9898320d18be114629ca1
TheHaruYoshida <santiagovidarte@gmail.com>
2023-01-12 18:04:27 -0800

    add task 9 without betty

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+  *
+  *
+  *
+  *
+  */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int height_left_tree = 0, height_right_tree = 0;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	height_left_tree = binary_tree_height(tree->left);
+	height_right_tree = binary_tree_height(tree->right);
+	if (height_left_tree >= height_right_tree)
+		return (height_left_tree + 1);
+	return (height_right_tree + 1);
+}
