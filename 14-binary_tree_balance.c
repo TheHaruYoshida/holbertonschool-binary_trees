2c6793e162d15132c9f53f0a88b618fcdfefd9b2
TheHaruYoshida <santiagovidarte@gmail.com>
2023-01-13 02:54:08 -0800

    add task 14 without betty

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+  *
+  *
+  *
+  *
+  */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height = 0, right_height = 0;
+
+	if (!tree)
+		return(0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	printf("left h = %d, right h = %d\n", left_height, right_height);
+	return (left_height - right_height);
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	 size_t left_height = 0, right_height = 0;
+
+	 if (tree == NULL)
+		return (0);
+	 if (tree->left)
+		 left_height += 1 + binary_tree_height(tree->left);
+	 if (tree->right)
+		 right_height += 1 + binary_tree_height(tree->right);
+	 if (left_height >= right_height)
+		return (left_height);
+	 return (right_height + 1);
+}
